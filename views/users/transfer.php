c72cc3e543856f45575789775eefdfaf62ad3837
Sp0ngbik <Sp0ngbik@gmail.com>
2022-02-16 17:49:39 +0300

    transfer php

---
+++ views/users/transfer.php
@@ -0,0 +1,15 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+
+<div class="site-login">
+<?php $form = ActiveForm::begin();?>
+    <?= $form->field($model, 'username')?>
+    <?= $form->field($model, 'usernameSend')?>
+    <?= $form->field($model, 'valueToSend')?>
+    <div class='form-group'>
+        <?= Html::submitButton('Save',['class'=>'btn btn-success'])?>
+    </div>
+    <?php ActiveForm::end()?>
+</div>
