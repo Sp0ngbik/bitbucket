570c32f08eb6145d7646c31ef5c59c4d9554fe37
Vlad Ostapuk <Sp0ngbik@gmail.com>
2022-02-18 21:58:57 +0300

    select form

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',    
 
     // Schema cache options (for production environment)
