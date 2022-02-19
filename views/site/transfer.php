7a27478659f0ed3a96ce9577aee7008bab50b926
Vlad Ostapuk <Sp0ngbik@gmail.com>
2022-02-20 01:17:24 +0300

    master

--- views/site/transfer.php
+++ views/site/transfer.php
@@ -14,10 +14,10 @@ use app\models\Users;
 
 <?= $form->field($model, 'currentUser')->dropDownList(
     ArrayHelper::map(Users::find()->all(), 'username', 'username','balance',)
-    )->label('Username who will send') ?>
+    )->label('Username that will send') ?>
       <?= $form->field($model, 'usernameSend')->dropDownList(
     ArrayHelper::map(Users::find()->all(), 'username', 'username','balance',)
-     )->label('Username who will send') ?>
+     )->label('Username that will send') ?>
     <?= $form->field($model, 'valueToSend')->label('Value to send')?>
     <?= $form->field( $model, 'password')->passwordInput()->label('Password of sender username')?>
     <div class='form-group'>
