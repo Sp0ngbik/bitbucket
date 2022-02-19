1fe2e0d76e85c22ba765b85be9a29cc42b0f916d
Vlad Ostapuk <Sp0ngbik@gmail.com>
2022-02-20 00:12:41 +0300

    fixes

--- views/site/transfer.php
+++ views/site/transfer.php
@@ -15,11 +15,11 @@ use app\models\NewUser;
         ],]);?>
      <?= $form->field($model, 'currentUser')->dropDownList(
     ArrayHelper::map(NewUser::find()->all(), 'username', 'username','balance',)
-    )->label('Username who will send') ?>
+    )->label('Username that will send') ?>
       <?= $form->field($model, 'usernameSend')->dropDownList(
     ArrayHelper::map(NewUser::find()->all(), 'username', 'username','balance',)
 
-     )->label('Username who will send') ?>
+     )->label('Username that will recieve') ?>
     <?= $form->field($model, 'valueToSend')->label('Value to send')?>
     <?= $form->field( $model, 'password')->passwordInput()->label('Password of sender username')?>
     <div class='form-group'>
