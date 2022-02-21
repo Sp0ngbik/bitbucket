433fe6063e9aaa4df60d4d9c82be46dbd8576e6c
Sp0ngbik <Sp0ngbik@gmail.com>
2022-02-21 15:15:46 +0300

    fixed master

--- views/site/transfer.php
+++ views/site/transfer.php
@@ -17,7 +17,7 @@ use app\models\Users;
     )->label('Username that will send') ?>
       <?= $form->field($model, 'usernameSend')->dropDownList(
     ArrayHelper::map(Users::find()->all(), 'username', 'username','balance',)
-     )->label('Username that will send') ?>
+     )->label('Username that will recieve') ?>
     <?= $form->field($model, 'valueToSend')->label('Value to send')?>
     <?= $form->field( $model, 'password')->passwordInput()->label('Password of sender username')?>
     <div class='form-group'>
